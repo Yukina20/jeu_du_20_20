3d06b3d755c920524f26079c00bdba41255f69cf
Victoria <victoriadubuc20@gmail.com>
2024-09-20 08:59:41 +0200

    Création des Entité Question Answer

--- src/Entity/Answer.php
+++ src/Entity/Answer.php
@@ -2,7 +2,98 @@
 
 namespace Dubuc\Entity;
 
+use DateTimeImmutable;
+
 class Answer
 {
+
+    private ?int $id;
+
+    private string $contentText;
+    private ?string $contentCode = null;
+    private ?string $contentImage = null;
+    private bool $isTrue = false;
+    private dateTimeImmutable $createdAt;
+    private ?dateTimeImmutable $revisedAt = null;
+
+    public function __construct()
+    {
+        $this->createdAt = new DateTimeImmutable();
+
+    }
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getContentText(): string
+    {
+        return $this->contentText;
+    }
+
+    public function setContentText(string $contentText): Answer
+    {
+        $this->contentText = $contentText;
+        return $this;
+    }
+
+    public function getContentCode(): ?string
+    {
+        return $this->contentCode;
+    }
+
+    public function setContentCode(?string $contentCode): Answer
+    {
+        $this->contentCode = $contentCode;
+        return $this;
+    }
+
+    public function getContentImage(): ?string
+    {
+        return $this->contentImage;
+    }
+
+    public function setContentImage(?string $contentImage): Answer
+    {
+        $this->contentImage = $contentImage;
+        return $this;
+    }
+
+    public function isTrue(): bool
+    {
+        return $this->isTrue;
+    }
+
+    public function setIsTrue(bool $isTrue): Answer
+    {
+        $this->isTrue = $isTrue;
+        return $this;
+    }
+
+    public function getCreatedAt(): DateTimeImmutable
+    {
+        return $this->createdAt;
+    }
+
+    public function setCreatedAt(DateTimeImmutable $createdAt): Answer
+    {
+        $this->createdAt = $createdAt;
+        return $this;
+    }
+
+    public function getRevisedAt(): ?DateTimeImmutable
+    {
+        return $this->revisedAt;
+    }
+
+    public function setRevisedAt(?DateTimeImmutable $revisedAt): Answer
+    {
+        $this->revisedAt = $revisedAt;
+        return $this;
+    }
+
+
+
 
 }
--- src/Entity/Question.php
+++ src/Entity/Question.php
@@ -2,7 +2,124 @@
 
 namespace Dubuc\Entity;
 
+use DateTimeImmutable;
+
 class Question
 {
+    private ?int $id;
+
+    private int $level;
+
+    private string $contentText;
+
+    private ?string $contentCode = null;
+
+    private ?string $contentImage = null;
+
+    private bool $isToBeRevised = false;
+
+    private DateTimeImmutable $createdAt;
+
+    private ?DateTimeImmutable $reviseAdt = null;
+
+    public function __construct()
+    {
+        $this->createdAt = new DateTimeImmutable();
+    }
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getLevel(): int
+    {
+        return $this->level;
+    }
+
+    public function setLevel(int $level): Question
+    {
+        $this->level = $level;
+
+        return $this;
+    }
+
+    public function getContentText(): string
+    {
+        return $this->contentText;
+    }
+
+    public function setContentText(string $contentText): Question
+    {
+        $this->contentText = $contentText;
+
+        return $this;
+    }
+
+    public function getContentCode(): ?string
+    {
+        return $this->contentCode;
+    }
+
+    public function setContentCode(?string $contentCode): Question
+    {
+        $this->contentCode = $contentCode;
+
+        return $this;
+    }
+
+    public function getContentImage(): ?string
+    {
+        return $this->contentImage;
+    }
+
+    public function setContentImage(?string $contentImage): Question
+    {
+        $this->contentImage = $contentImage;
+
+        return $this;
+    }
+
+    public function isToBeRevised(): bool
+    {
+        return $this->isToBeRevised;
+    }
+
+    public function setIsToBeRevised(bool $isToBeRevised): Question
+    {
+        $this->isToBeRevised = $isToBeRevised;
+
+        return $this;
+    }
+
+    public function getCreatedAt(): DateTimeImmutable
+    {
+        return $this->createdAt;
+    }
+
+    public function setCreatedAt(DateTimeImmutable $createdAt): Question
+    {
+        $this->createdAt = $createdAt;
+
+        return $this;
+    }
+
+    public function getReviseAdt(): ?DateTimeImmutable
+    {
+        return $this->reviseAdt;
+    }
+
+    public function setReviseAdt(?DateTimeImmutable $reviseAdt): Question
+    {
+        $this->reviseAdt = $reviseAdt;
+
+        return $this;
+    }
+
+
+
+
+
+
 
 }
