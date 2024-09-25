dda8b9402719da07dfc9637053699d751ecbaeae
Victoria <victoriadubuc20@gmail.com>
2024-09-25 16:20:47 +0200

    Create BaseController class with renderViewTemp method declaration

---
+++ src/Controller/BaseController.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace Dubuc\Controller;
+use Dubuc\Utils\ViewRender;
+
+class BaseController
+{
+
+    public static function renderViewTemp(
+        string $viewDefinition,
+        array $data = []
+    ): void {
+
+    }
+
+}
